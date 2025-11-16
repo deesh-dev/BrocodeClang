83c2eb31668059c0f5f357a4990f3b20f75c761d
Deepak Kumar <workforudeepak@gmail.com>
2025-11-16 09:15:37 +0530

    Improved with switch statement instead of conditional

--- calculator/main.c
+++ calculator/main.c
@@ -9,10 +9,26 @@ int main(void){
 
     scanf("%lf %c %lf", &num1, &op, &num2);
 
-    if (op == '+') printf("%.4lf + %.4lf = %.4lf\n", num1, num2, num1 + num2);
-    else if (op == '-') printf("%.4lf - %.4lf = %.4lf\n", num1, num2, num1 - num2);
-    else if (op == '/') printf("%.4lf / %.4lf = %.4lf\n", num1, num2, num1 / num2);
-    else if (op == '*') printf("%.4lf * %.4lf = %.4lf\n", num1, num2, num1 * num2);
+    switch (op)
+    {
+    case '+':
+        printf("%.4lf + %.4lf = %.4lf\n", num1, num2, num1 + num2);
+        break;
+    
+    case '-':
+        printf("%.4lf - %.4lf = %.4lf\n", num1, num2, num1 - num2);
+        break;
 
+    case '*':
+        printf("%.4lf / %.4lf = %.4lf\n", num1, num2, num1 / num2);
+        break;
+    
+    case '/':
+        printf("%.4lf * %.4lf = %.4lf\n", num1, num2, num1 * num2);
+        break;
+        
+    default:
+        printf("%c is not a valid operation\n", op);
+    }
     return 0;
 }
